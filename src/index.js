86794edeade9444c5ce607c85a5b550d6f7e895f
Dean Ditton <deansteven308@gmail.com>
2018-02-12 16:35:56 +1000

    added new password to api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import {authenticationInit} from "./reducer";
 import {submitAuthUserAction} from "./events/authenticateUser";
 import {logoutAction} from "./events/logoutUser";
+import {submitNewPasswordAction} from "../dist/events/newPassword";
+
+// Reducer Hook
+export const authenticationReducers = authenticationInit
 
 // Action API
-export const authenticationReducers = authenticationInit
 export const submitLogin = submitAuthUserAction
 export const submitLogout = logoutAction
+export const submitnewPassword = submitNewPasswordAction
