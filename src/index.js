3f51dae972d1c6ac6ebf74184a2fccf4cf461e84
Dean Ditton <deansteven308@gmail.com>
2018-02-13 14:01:11 +1000

    changed how exports work

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import {submitAuthUserAction} from "./events/authenticateUser";
 import {logoutAction} from "./events/logoutUser";
 import {submitNewPasswordAction} from "../dist/events/newPassword";
 
-// Reducer Hook
-export const authenticationReducers = authenticationInit
 
 // Action API
-export const submitLogin = submitAuthUserAction
-export const submitLogout = logoutAction
-export const submitnewPassword = submitNewPasswordAction
+export {
+    authenticationInit, //Reducer builder
+    submitAuthUserAction,
+    logoutAction,
+    submitNewPasswordAction
+}
